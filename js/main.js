c103547e833b5d86a1403efccd2a643037e1ecd8
Robert Nelson <robert.nelson@Bobs-Work-Mac.local>
2014-09-23 16:19:27 -0700

    Almost done... just about where I want it. On to clean up.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
-var callback = function(){
+var init = function(){
 	$('.item-skills').each(function(){
 		newWidth = $(this).parent().width() * $(this).data('percent');
 		$(this).width(0);
-    $(this).animate({
-        width: newWidth,
-    }, 1000);
+	    $(this).animate({
+	        width: newWidth,
+	    }, 1000);
 	});
-	$('.icons-red').each(function(){
+	
+	$('.icons-blue').each(function(){
 		height = $(this).height();
-    $(this).animate({
-        height: 14,
-    }, 2000);
+    	$(this).animate({
+        	height: 14,
+    	}, 1000);
+	});
+	
+	$('a.accomps').on('click', function(e){ 
+		e.preventDefault();
+		$(e.target).parent().next('ul').slideToggle();
 	});
 };
-$(document).ready(callback);
+$(document).ready(init);
 
-var resize;
+/*var resize;
 window.onresize = function() {
 	clearTimeout(resize);
 	resize = setTimeout(function(){
 		callback();
 	}, 100);
-};
+};*/
